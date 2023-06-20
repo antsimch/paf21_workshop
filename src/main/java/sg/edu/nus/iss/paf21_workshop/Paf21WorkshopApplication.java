bb955af1b6e709f775a99550c92e4029a7869e1f
antsimch <anthonysimch@gmail.com>
2023-06-20 12:25:39 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/paf21_workshop/Paf21WorkshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf21_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paf21WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paf21WorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/paf21_workshop/Paf21WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf21_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paf21WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
